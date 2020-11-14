5ef7a9dc3a5d920b56c18f5777eb5825a46a45a2
mild-tn <tassaneewan.mild@mail.kmutt.ac.th>
2020-11-15 02:57:23 +0700

    :lipstick: Add card of base component 🌻

---
+++ core/components/Card/Card.tsx
@@ -0,0 +1,38 @@
+import { Card as MaterialCard, Typography } from '@material-ui/core'
+
+import React from 'react'
+
+export interface CardProps {
+  className?: string
+  tags: unknown
+  srcImg: string
+  title?: string
+  aboutUs?: string
+}
+
+export const Card = ({ className, tags, srcImg, aboutUs, title }) => (
+  <>
+    <MaterialCard style={{ height: '199px' }} className={`${className} w-full flex pr-4`}>
+      <div className="w-1/3 mr-3">
+        <img src={srcImg} alt="Normal Card" />
+      </div>
+      <div className="my-6 align-middle">
+        <Typography className="mb-3 ml-1 font-bold text-primary font-prompt text-heading-6">
+          {title}
+        </Typography>
+        <div className="mt-2">
+          {tags.map((data, i) => (
+            <span
+              key={i}
+              className="px-1 mr-2 text-sm font-normal text-black bg-opacity-50 rounded text-body-2 bg-secondary2">
+              {data}
+            </span>
+          ))}
+        </div>
+        <div className="mt-2 text-black text-body-2">
+          <span className="text-subtitle-2">{aboutUs}</span>
+        </div>
+      </div>
+    </MaterialCard>
+  </>
+)
---
+++ core/components/Card/Small.tsx
@@ -0,0 +1,44 @@
+import { Business, QueryBuilder } from '@material-ui/icons'
+import { Card as MaterialCard, Typography } from '@material-ui/core'
+
+import React from 'react'
+
+export interface CardProps {
+  className?: string
+  tags: unknown
+  srcImg: string
+  title?: string
+  date?: string
+  company?: string
+}
+
+export const CardSmall = ({ className, tags, srcImg, title, date, company }) => (
+  <>
+    <MaterialCard style={{ height: '163px' }} className={`${className} w-1/3 flex p-2`}>
+      <div className="mr-3">
+        <img width="80px" src={srcImg} alt="Small Card" />
+      </div>
+      <div className="align-middle">
+        <Typography className="mb-3 ml-1 font-bold text-primary font-prompt text-heading-6">
+          {title}
+        </Typography>
+        {tags.map((data, i) => (
+          <span
+            key={i}
+            className="px-1 mr-2 text-sm font-normal text-black bg-opacity-50 rounded text-body-2 bg-secondary2">
+            {data}
+          </span>
+        ))}
+        <br />
+        <div className="mt-1 text-black text-body-2">
+          <Business className="mb-2 mr-2" />
+          <span className="text-subtitle-2">{company}</span>
+        </div>
+        <div className="text-black text-body-2">
+          <QueryBuilder className="mb-2 mr-2" />
+          <span className="mt-2 text-subtitle-2">{date}</span>
+        </div>
+      </div>
+    </MaterialCard>
+  </>
+)
