df9a7f2f2fca13cf618d59077115979f043e1b39
kaotu <kao-tu@hotmail.co.th>
2020-11-21 01:20:51 +0700

    🔨  Add build and push images script 🌚

---
+++ push.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo Build images..
+docker build -t 192479299400.dkr.ecr.ap-southeast-1.amazonaws.com/sit-careers:fe-dev -f docker/app/Dockerfile-dev .
+docker build -t 192479299400.dkr.ecr.ap-southeast-1.amazonaws.com/sit-careers:fe-nginx-dev -f docker/nginx/Dockerfile .
+
+echo Push images..
+docker push 192479299400.dkr.ecr.ap-southeast-1.amazonaws.com/sit-careers:fe-nginx-dev
+docker push 192479299400.dkr.ecr.ap-southeast-1.amazonaws.com/sit-careers:fe-dev
