c8ebcae23bc0bd8d678562e092249b1915c73d50
mild-tn <tassaneewan.mild@mail.kmutt.ac.th>
2020-12-10 17:13:32 +0700

    :lipstick: Check logic for searching when find not found 🌻

--- features/Company/pages/company-info.tsx
+++ features/Company/pages/company-info.tsx
@@ -53,21 +53,29 @@ const CompanyInfo = () => {
           }}
         />
       </div>
-      {context.companies
-        .slice(contextPagination.sliceDataStart, contextPagination.sliceDataEnd)
-        .map((data, i) => {
-          return (
-            <Card
-              className="my-3"
-              key={i}
-              title={`${data.company_name_th} - ${data.company_name_en}`}
-              tags={data.mou_link ? [`${data.company_type}`, 'MOU'] : [`${data.company_type}`]}
-              aboutUs={data.description}
-              srcImg="https://i.picsum.photos/id/1000/5626/3635.jpg?hmac=qWh065Fr_M8Oa3sNsdDL8ngWXv2Jb-EE49ZIn6c0P-g"
-            />
-          )
-        })}
-      <Pagination data={context.companies} />
+      {context.companies.length !== 0 ? (
+        <>
+          {context.companies
+            .slice(contextPagination.sliceDataStart, contextPagination.sliceDataEnd)
+            .map((data, i) => {
+              return (
+                <Card
+                  className="my-3"
+                  key={i}
+                  title={`${data.company_name_th} - ${data.company_name_en}`}
+                  tags={data.mou_link ? [`${data.company_type}`, 'MOU'] : [`${data.company_type}`]}
+                  aboutUs={data.description}
+                  srcImg="https://i.picsum.photos/id/1000/5626/3635.jpg?hmac=qWh065Fr_M8Oa3sNsdDL8ngWXv2Jb-EE49ZIn6c0P-g"
+                />
+              )
+            })}
+          <Pagination data={context.companies} />
+        </>
+      ) : (
+        <div className="flex flex-col items-center justify-center w-full h-16">
+          <span className="font-prompt text-heading-6">ไม่พบผลการค้นหา</span>
+        </div>
+      )}
     </div>
   ))
 }
