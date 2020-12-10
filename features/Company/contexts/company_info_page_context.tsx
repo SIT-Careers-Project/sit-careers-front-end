dc4820fd8ac1f9072bbb55026a91c35ca2e1773e
mild-tn <tassaneewan.mild@mail.kmutt.ac.th>
2020-12-10 16:14:21 +0700

    :recycle: Refactoring code 🌻

--- features/Company/contexts/company_info_page_context.tsx
+++ features/Company/contexts/company_info_page_context.tsx
@@ -5,19 +5,28 @@ import { createContext } from 'react'
 
 export class CompanyInfoPageContext {
   companies
+  beforeSearch
 
   constructor() {
     makeObservable(this, {
       getCompanies: action,
-      companies: observable
+      setCompanies: action,
+      companies: observable,
+      beforeSearch: observable
     })
     this.companies = []
+    this.beforeSearch = []
+  }
+
+  setCompanies = (companies) => {
+    this.companies = companies
   }
 
   getCompanies = async () => {
     try {
       const response = await apiService.getAllCompanies()
       this.companies = response.data
+      this.beforeSearch = response.data
     } catch (error) {
       console.log(error)
     }
