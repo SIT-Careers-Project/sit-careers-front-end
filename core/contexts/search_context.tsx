f70fab7bcfd36ff6fb54204e7b674548c2355ad1
jirattikarn <jirattikarn.vil@mail.kmutt.ac.th>
2021-01-01 22:35:49 +0700

    Create searchMultiFilter cr.🌻

--- core/contexts/search_context.tsx
+++ core/contexts/search_context.tsx
@@ -1,23 +1,33 @@
 import { action, makeObservable, observable } from 'mobx'
-
 import { createContext } from 'react'
 import { matchSorter } from 'match-sorter'
-
 export class SearchContext {
   items
-
   constructor() {
     makeObservable(this, {
       setSearchItems: action,
-      items: observable
+      items: observable,
+      searchMultiFilter: action
     })
   }
-
   setSearchItems = (value, items, keySearch) => {
+    console.log(value)
     return matchSorter(items, value, {
       keys: keySearch
     })
   }
+  matchSorterAcrossKeys = (list, search, options) => {
+    const joinedKeysString = (item) => options.keys.map((key) => item[key]).join(' ')
+    return matchSorter(list, search, {
+      ...options,
+      keys: [...options.keys, joinedKeysString]
+    })
+  }
+  searchMultiFilter = (value, items, keySearch) => {
+    const search = this.matchSorterAcrossKeys(items, value, {
+      keys: keySearch
+    })
+    return search
+  }
 }
-
 export const searchContext = createContext(new SearchContext())
