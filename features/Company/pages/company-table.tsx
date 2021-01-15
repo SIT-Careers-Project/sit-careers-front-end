7ae88e0961d38f33e38c61538c64aa801b2b4495
mild-tn <tassaneewan.mild@mail.kmutt.ac.th>
2021-01-15 19:17:07 +0700

    Add name of company 🌻

--- features/Company/pages/company-table.tsx
+++ features/Company/pages/company-table.tsx
@@ -18,29 +18,40 @@ const CompanyTable = () => {
 
   const column = [
     {
-      title: 'Logo',
-      field: 'logo',
+      title: 'บริษัท',
+      field: 'company_name_th',
       render: function (rowData) {
         return (
           <>
             {rowData.logo === '-' ? (
-              <div className="flex items-center justify-center w-10 h-10 rounded-full bg-secondary1">
-                <span className="text-white text-heading-6">
-                  {rowData.company_name_en.substr(0, 1)}
-                </span>
+              <div className="flex flex-row">
+                <div className="flex flex-row items-center justify-center w-10 h-10 rounded-full bg-secondary1">
+                  <span className="text-white text-heading-6">
+                    {rowData.company_name_en.substr(0, 1)}
+                  </span>
+                </div>
+                <div className="ml-5">
+                  <p className="font-sarabun-samibold">{rowData.company_name_th}</p>
+                  <p className="text-body-2 text-secondary2">{rowData.company_name_en}</p>
+                </div>
               </div>
             ) : (
-              <img
-                alt="logo"
-                src={`${publicRuntimeConfig.s3_url}/logo/${rowData.logo}`}
-                style={{ width: '40px', borderRadius: '50%' }}
-              />
+              <div className="flex flex-row">
+                <img
+                  alt="logo"
+                  src={`${publicRuntimeConfig.s3_url}/logo/${rowData.logo}`}
+                  style={{ width: '40px', borderRadius: '50%' }}
+                />
+                <div className="ml-5">
+                  <p className="font-sarabun-samibold">{rowData.company_name_th}</p>
+                  <p className="text-body-2 text-secondary2">{rowData.company_name_en}</p>
+                </div>
+              </div>
             )}
           </>
         )
       }
     },
-    { title: 'บริษัท', field: 'company_name_en' },
     { title: 'ประเภทธุรกิจ', field: 'company_type' },
     {
       title: 'MOU',
@@ -54,14 +65,14 @@ const CompanyTable = () => {
       field: 'company_id',
       render: (rowData) => (
         <Link href={`/company/update/${rowData.company_id}`}>
-          <SettingsApplications className="cursor-pointer text-secondary2" fontSize="large" />
+          <SettingsApplications className="cursor-pointer text-secondary1" fontSize="large" />
         </Link>
       )
     }
   ]
 
   return (
-    <div className="w-full h-full max-w-screen-lg mb-16 bg-white">
+    <div className="w-full h-auto max-w-screen-lg mb-16 bg-white">
       <div className="flex justify-between w-full mt-2">
         <div>
           <p className="text-heading-5 font-prompt">จัดการข้อมูลบริษัท</p>
@@ -86,7 +97,8 @@ const CompanyTable = () => {
               data={context.companies}
               options={{
                 showTitle: false,
-                draggable: false
+                draggable: false,
+                search: true
               }}
             />
           )}
