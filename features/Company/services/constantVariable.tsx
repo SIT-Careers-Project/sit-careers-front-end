bd616789477c989099bb8b109bceef2c0a464c75
mild-tn <tassaneewan.mild@mail.kmutt.ac.th>
2021-01-28 15:04:33 +0700

    :recycle: Refactoring frequently used variable of company feature

---
+++ features/Company/services/constantVariable.tsx
@@ -0,0 +1,17 @@
+export const companyType = [
+  { title: 'Software House' },
+  { title: 'Technology Consultant' },
+  { title: 'Big data' },
+  { title: 'DevOps' },
+  { title: 'Cloud' }
+]
+
+export const days = [
+  { day: 'จันทร์' },
+  { day: 'อังคาร' },
+  { day: 'พุธ' },
+  { day: 'พฤหัสบดี' },
+  { day: 'ศุกร์' },
+  { day: 'เสาร์' },
+  { day: 'อาทิตย์' }
+]
