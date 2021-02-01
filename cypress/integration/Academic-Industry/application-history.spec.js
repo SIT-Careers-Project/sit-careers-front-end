e5a8504e07cdd028de7da4bcf5c50863f175c32e
jirattikarn <jirattikarn.vil@mail.kmutt.ac.th>
2021-02-01 22:22:27 +0700

    :white_check_mark: Write test for application history 🐯

---
+++ cypress/integration/Academic-Industry/application-history.spec.js
@@ -0,0 +1,15 @@
+describe('The Application History Page', () => {
+  it('successfully loads', () => {
+    cy.visit('/academic-industry/application')
+  })
+  it('This page contains navbar component', () => {
+    cy.get('[data-cy=core-navbar]').should('be.visible')
+  })
+  it('It is a have footer component', () => {
+    cy.get('[data-cy=core-footer]').should('be.visible')
+  })
+  it('should show header ประวัติการสมัครงาน and application history tables.', () => {
+    cy.contains('ประวัติการสมัครงาน')
+    cy.get('.MuiTableHead-root ')
+  })
+})
