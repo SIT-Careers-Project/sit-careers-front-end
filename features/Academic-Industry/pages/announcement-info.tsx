4a4660299cacff3368adc7f1a7086ad8fbc260a3
jirattikarn <jirattikarn.vil@mail.kmutt.ac.th>
2021-02-04 17:16:25 +0700

    getAnnouncements to show on info page 🐯

--- features/Academic-Industry/pages/announcement-info.tsx
+++ features/Academic-Industry/pages/announcement-info.tsx
@@ -1,13 +1,22 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect } from 'react'
 import { useObserver } from 'mobx-react-lite'
 import { Announcement } from '../components/Announcement'
 import { AddCircle } from '@material-ui/icons'
 import Link from 'next/link'
 import Search from '../../../core/components/Search'
 import { announcementDuplicateFormContext } from '../context/announcement_duplicate_form_context'
+import { announcementInfoContext } from '../context/announcement_info_context'
+import { searchContext } from '../../../core/contexts/search_context'
+import Pagination from '../../../core/components/Pagination'
 
 const CompanyInfo = () => {
   const context = useContext(announcementDuplicateFormContext)
+  const contextInfo = useContext(announcementInfoContext)
+  const contextSearch = useContext(searchContext)
+
+  useEffect(() => {
+    contextInfo.getAnnouncements()
+  }, [contextInfo])
 
   return useObserver(() => (
     <div className="w-full h-full max-w-screen-lg mb-16">
@@ -28,17 +37,44 @@ const CompanyInfo = () => {
       </div>
       <div className="w-full h-1 mt-5 mb-5 bg-secondary1" />
       <div className="w-full h-8 bg-grey-100">
-        <Search />
-      </div>
-      <div className="pt-5">
-        <Announcement
-          title="รับสมัครงานตำแหน่ง Software Engineer"
-          tags={['Software Engineer', 'WiL']}
-          date="12 มีนาคม - 24 ธันวาคม 2563"
-          company="SIT Company"
-          srcImg="https://avatars0.githubusercontent.com/u/22110844?s=280&v=4"
-          context={context}
+        <Search
+          onChange={(event) => {
+            if (typeof event.target.value === 'string') {
+              const keySearch = ['announcement_title', 'company_name_en', 'company_name_th']
+              contextInfo.setAnnouncements(
+                contextSearch.setSearchItems(
+                  [event.target.value],
+                  contextInfo.beforeSearch,
+                  keySearch
+                )
+              )
+            }
+          }}
         />
+      </div>
+      <div>
+        {contextInfo.announcements.length !== 0 ? (
+          <>
+            {contextInfo.announcements.map((data, i) => {
+              return (
+                <div className="pt-5" key={i}>
+                  <Announcement
+                    title={data.announcement_title}
+                    tags={[data.job_position, data.job_type, data.status]}
+                    date={`${data.start_date} - ${data.end_date}`}
+                    srcImg="https://i.picsum.photos/id/1000/5626/3635.jpg?hmac=qWh065Fr_M8Oa3sNsdDL8ngWXv2Jb-EE49ZIn6c0P-g"
+                    context={context}
+                  />
+                </div>
+              )
+            })}
+            <Pagination data={contextInfo.announcements} />
+          </>
+        ) : (
+          <div className="flex flex-col items-center justify-center w-full h-16">
+            <span className="font-prompt text-heading-6">ไม่พบผลลัพธ์</span>
+          </div>
+        )}
       </div>
     </div>
   ))
