f88bb1c32390032c50200889c14c2d15913b4627
mild-tn <tassaneewan.mild@mail.kmutt.ac.th>
2021-02-07 14:50:11 +0700

    Adjust style of banner upload image 🌻

--- core/components/BannerImage.tsx
+++ core/components/BannerImage.tsx
@@ -10,10 +10,10 @@ export const BannerImages = (props: BannerImage) => {
   const { className, imgSrc } = props
   return (
     <div
-      className={`w-1/2 rounded-none h-40 flex justify-end items-end ${className}`}
+      className={`w-full rounded-none h-48 flex justify-end items-end ${className}`}
       style={{ backgroundImage: `url(${imgSrc})` }}>
-      <div className="ml-8 text-secondary2">
-        <CameraAlt fontSize="large" />
+      <div className="flex flex-col items-center justify-center w-8 h-8 -mb-3 -mr-3 text-white rounded-full bg-secondary1">
+        <CameraAlt fontSize="small" />
       </div>
     </div>
   )
