4c4840052c0625dbaa14421e6431218161b3b1e4
mild-tn <tassaneewan.mild@mail.kmutt.ac.th>
2021-02-13 13:35:59 +0700

    :bug: Fix style of small card 🌻

--- core/components/Card/Small.tsx
+++ core/components/Card/Small.tsx
@@ -1,6 +1,6 @@
 import { Business, QueryBuilder } from '@material-ui/icons'
-import { Card as MaterialCard } from '@material-ui/core'
 
+import { Card as MaterialCard } from '@material-ui/core'
 import React from 'react'
 
 interface CardProps {
@@ -13,30 +13,30 @@ interface CardProps {
 }
 
 export const CardSmall = ({ className, tags, srcImg, title, date, company }: CardProps) => (
-  <div className="pb-3">
-    <MaterialCard style={{ height: '163px' }} className={`${className} flex p-2`}>
-      <div className="mr-3">
+  <div>
+    <MaterialCard
+      style={{ height: '201px', width: '403px' }}
+      className={`${className} grid grid-cols-4 p-4 gap-4`}>
+      <div className="col-span-1">
         <img width="80px" src={srcImg} alt="Small Card" />
       </div>
-      <div className="align-middle">
-        <p className="mb-3 ml-1 font-bold leading-6 font-prompt text-primary text-heading-6">
-          {title}
-        </p>
+      <div className="col-span-3 align-middle">
+        <p className="mb-3 font-bold leading-6 font-prompt text-primary text-heading-6">{title}</p>
         {tags.map((data, i) => (
           <span
             key={i}
-            className="px-1 mr-2 text-sm font-normal text-black bg-opacity-50 rounded text-body-2 bg-secondary2">
+            className="px-1 mr-2 text-sm font-normal text-white rounded text-body-2 bg-primary">
             {data}
           </span>
         ))}
         <br />
         <div className="mt-1 text-black text-body-2">
           <Business className="mb-2 mr-2" />
-          <span className="text-subtitle-2">{company}</span>
+          <span className="text-body-1">{company}</span>
         </div>
         <div className="text-black text-body-2">
           <QueryBuilder className="mb-2 mr-2" />
-          <span className="mt-2 text-subtitle-2">{date}</span>
+          <span className="text-body-1">{date}</span>
         </div>
       </div>
     </MaterialCard>
