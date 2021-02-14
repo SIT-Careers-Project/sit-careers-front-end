3eb66bf9429aabbffdabba8d00ad89b4873b2c30
jirattikarn <jirattikarn.vil@mail.kmutt.ac.th>
2021-02-14 21:17:10 +0700

    :ok_hand: Add style inline 🐯

--- core/components/TabPanel.tsx
+++ core/components/TabPanel.tsx
@@ -1,28 +1,17 @@
 import React from 'react'
 import { Box, Typography } from '@material-ui/core'
-import { makeStyles } from '@material-ui/core/styles'
 
 export const TabPanel = (props) => {
   const { value, index, children } = props
-  const useStyles = makeStyles(() => ({
-    tab: {
-      '& .MuiBox-root': {
-        padding: '0px'
-      }
-    }
-  }))
-
-  const classes = useStyles()
 
   return (
     <div
-      className={classes.tab}
       role="tabpanel"
       hidden={value !== index}
       id={`simple-tabpanel-${index}`}
       aria-labelledby={`simple-tab-${index}`}>
       {value === index && (
-        <Box p={3}>
+        <Box p={3} style={{ padding: '0px' }}>
           <Typography>{children}</Typography>
         </Box>
       )}
