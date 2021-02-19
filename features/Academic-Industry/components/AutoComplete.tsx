699e08caffac42bdd204c2c9a6a7fb668328176d
mild-tn <tassaneewan.mild@mail.kmutt.ac.th>
2021-02-20 00:15:39 +0700

    :lipstick: Update autocomplete component 🌻

--- features/Academic-Industry/components/AutoComplete.tsx
+++ features/Academic-Industry/components/AutoComplete.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 import { Autocomplete as MaterialAutoComplete } from '@material-ui/lab'
 import { TextField } from '@material-ui/core'
@@ -13,28 +13,53 @@ type CoreAutoCompleteProps = {
   label: string
   error: boolean
   helperText: string
+  defaultValue?: any
 }
 
 export const AutoComplete = (props: CoreAutoCompleteProps) => {
-  const [value, setValue] = React.useState('')
-  const { options, keySearch, inputRef, keyName, className, label, error, helperText } = props
+  const [value, setValue] = useState('')
+  const [inputValue, setInputValue] = useState('')
+
+  const {
+    options,
+    keySearch,
+    inputRef,
+    keyName,
+    className,
+    label,
+    error,
+    helperText,
+    defaultValue
+  } = props
 
   const ref = useRef(null)
 
   // eslint-disable-next-line @typescript-eslint/no-empty-function
-  useEffect(() => {}, [keySearch, keyName])
+  useEffect(() => {
+    if (defaultValue) {
+      const dataFilter = options.filter((data) => data[keyName] === defaultValue)
+      setValue(dataFilter[0][keyName])
+      setInputValue(dataFilter[0][keySearch])
+    }
+  }, [keySearch, keyName, defaultValue, options])
 
   return (
     <div className={className}>
       <MaterialAutoComplete
         options={options}
-        ref={ref}
+        ref={ref.current}
+        includeInputInList={true}
+        onInputChange={(event) => {
+          setInputValue(event.target?.value)
+        }}
         onChange={(event, newValue) => {
           if (newValue) {
             setValue(newValue[keyName])
+            setInputValue(newValue[keySearch])
           }
         }}
         getOptionLabel={(option) => option[keySearch]}
+        inputValue={inputValue}
         renderInput={(params) => (
           <TextField
             {...params}
