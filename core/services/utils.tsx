979966483e2668f72655a8ae79f170f679c4c40e
mild-tn <tassaneewan.mild@mail.kmutt.ac.th>
2021-03-02 17:29:29 +0700

    :truck: Move utils file to service on core folder 🌻

--- core/services/utils.tsx
+++ core/services/utils.tsx
@@ -2,7 +2,10 @@ import isBetween from 'dayjs/plugin/isBetween'
 import dayjs from 'dayjs'
 import _ from 'lodash'
 
-export const checkStatus = (startDate, endDate) => {
+export const checkStatus = (startDate, endDate, status) => {
+  if (status === 'CLOSE') {
+    return 'CLOSE'
+  }
   dayjs.extend(isBetween)
   if (dayjs().isBetween(startDate, endDate, null, '[)')) {
     return 'OPEN'
