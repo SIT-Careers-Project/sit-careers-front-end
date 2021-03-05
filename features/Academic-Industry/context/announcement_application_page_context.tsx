98df0931773b5673805bdc018e95f921ffcd6342
mild-tn <tassaneewan.mild@mail.kmutt.ac.th>
2021-03-05 19:05:54 +0700

    :sparkles: Update adding get annoucenment and create application method 🌻

--- features/Academic-Industry/context/announcement_application_page_context.tsx
+++ features/Academic-Industry/context/announcement_application_page_context.tsx
@@ -1,19 +1,41 @@
-import { action, makeObservable, observable } from 'mobx'
+import { makeAutoObservable } from 'mobx'
 
 import { createContext } from 'react'
-
+import apiAcademic from '../services/apiAcademicIndustry'
+import Router from 'next/router'
 export class AnnouncementApplicationFormContext {
   modal
+  announcement
 
   constructor() {
-    makeObservable(this, {
-      modal: observable,
-      keyChange: action
-    })
+    this.modal = ''
+    this.announcement = ''
+
+    makeAutoObservable(this)
   }
 
   keyChange = (key, value) => {
     this[key] = value
+  }
+
+  getAnnouncementById = async (announcementId) => {
+    try {
+      const response = await apiAcademic.getAnnouncementById(announcementId)
+      this.announcement = response.data
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  createApplication = async (data) => {
+    try {
+      await apiAcademic.createApplication(data).then(() => {
+        this.modal.closeModal()
+        Router.push('/academic-industry/applications/history')
+      })
+    } catch (error) {
+      console.log(error)
+    }
   }
 }
 export const announcementApplicationFormContext = createContext(
