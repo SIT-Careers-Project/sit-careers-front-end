9f608e6f151f723e273f4c26095b60b0040e4d6c
jirattikarn <jirattikarn.vil@mail.kmutt.ac.th>
2021-03-31 00:48:43 +0700

    Create constant variable of resume 🐯

---
+++ features/Resume/services/constantVariable.tsx
@@ -0,0 +1,9 @@
+export const nameTitle = [{ title: 'นาย' }, { title: 'นางสาว' }, { title: 'นาง' }]
+
+export const curriculum = [
+  { title: 'เทคโนโลยีสารสนเทศ (IT)', value: 'IT' },
+  { title: 'วิทยาการคอมพิวเตอร์ (CS)', value: 'CS' },
+  { title: 'นวัตกรรมบริหารดิจิทัล (DSI)', value: 'DSI' }
+]
+
+export const years = [{ title: '1' }, { title: '2' }, { title: '3' }, { title: '4' }]
