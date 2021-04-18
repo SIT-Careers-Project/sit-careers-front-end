e9d91c43a18ceebda3aedbf277af8aead8bc919e
mild-tn <tassaneewan.mild@mail.kmutt.ac.th>
2021-04-18 20:52:14 +0700

    :lipstick: Update permission of info-management to access this page 🌻

--- pages/academic-industry/info-management.tsx
+++ pages/academic-industry/info-management.tsx
@@ -1,15 +1,30 @@
 import AnnouncementPage from '../../features/Academic-Industry/pages/announcement-info'
-import { MainLayout } from '../../core/components/Layout/Main'
-import React from 'react'
+import { MainLayout } from 'core/components/Layout/Main'
+import React, { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import { checkLoggedIn } from 'core/services/utils'
 
-const CompanyInfo = ({ authContext }) => {
+const InfoManagementPage = ({ authContext }) => {
+  const router = useRouter()
+
+  useEffect(() => {
+    authContext.fetchMe().then(() => {
+      const path = checkLoggedIn(
+        authContext.isLoggedIn,
+        ['admin', 'manager', 'coordinator'],
+        authContext.roleUser
+      )
+      path && router.push(path)
+    })
+  }, [authContext, router])
+
   return (
     <MainLayout authContext={authContext}>
       <div className="flex justify-center mt-16">
-        <AnnouncementPage />
+        <AnnouncementPage authContext={authContext} />
       </div>
     </MainLayout>
   )
 }
 
-export default CompanyInfo
+export default InfoManagementPage
