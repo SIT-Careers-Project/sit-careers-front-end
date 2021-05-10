a411987931ad3a210011d440b130d4905db4cae4
jirattikarn <jirattikarn.vil@mail.kmutt.ac.th>
2021-05-11 02:12:38 +0700

    import createReport to use in apiService 🐯

--- features/Report-Management/services/apiReport.tsx
+++ features/Report-Management/services/apiReport.tsx
@@ -1,22 +1,10 @@
 import api from '../../../utils/api'
 
 const apiReport = {
-  getCompaniesByFilterDate: async (startDate, endDate) => {
-    const response = await api.get(
-      `/dashboard/companies/export?start_date=${startDate}&end_date=${endDate}`
-    )
-    return response
-  },
-  getAnnouncementsByFilterDate: async (startDate, endDate) => {
-    const response = await api.get(
-      `/dashboard/announcements/export?start_date=${startDate}&end_date=${endDate}`
-    )
-    return response
-  },
-  getDashboardByFilterDate: async (startDate, endDate) => {
-    const response = await api.get(
-      `/dashboard/dashboard/export?start_date=${startDate}&end_date=${endDate}`
-    )
+  createReport: async (data) => {
+    const response = await api.post('/dashboard/report', data, {
+      headers: { 'Content-type': 'application/octet-stream' }
+    })
     return response
   }
 }
