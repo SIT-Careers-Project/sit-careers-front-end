2d4f71cf55d5d80347ec1d60f30892b6d8f77da4
Mild-TN <tassaneewan.mild@mail.kmutt.ac.th>
2021-08-11 18:52:15 +0700

    Update modal_banner_image_context.tsx

--- core/contexts/modal_banner_image_context.tsx
+++ core/contexts/modal_banner_image_context.tsx
@@ -1,4 +1,4 @@
-import { makeAutoObservable, toJS } from 'mobx'
+import { makeAutoObservable } from 'mobx'
 import { createContext } from 'react'
 import { apiBanner } from '../services/apiBanner'
 import _ from 'lodash'
