37fbc6c5793ed3bac6d8287084177e217a7fb2f9
Mild-TN <21-mild-tn@users.noreply.git.innovasive.co.th>
2021-10-16 14:08:18 +0700

    :bug: Fix search by compnay  name 🌻

--- core/contexts/search_context.tsx
+++ core/contexts/search_context.tsx
@@ -36,11 +36,16 @@ export class SearchContext {
   searchMultiFilterWithFuse = (items, searchValue, keySearch) => {
     let data = []
     const flattenSearchValue = _.without(_.flattenDeep(searchValue), '')
-    const keys = _.uniq(keySearch)
+    const byCompanyName = this.searchByCompanyName(keySearch, items, flattenSearchValue)
+
+    const keys = _.sortedUniq(toJS(keySearch))
     const options = {
       minMatchCharLength: 28,
       includeScore: true,
-      keys: keys
+      keys: _.remove(
+        toJS(keys),
+        (array) => array.name === 'company_name_th' || array.name === 'company_name_en'
+      )
     }
     const fuse = new Fuse(toJS(items), options)
     if (flattenSearchValue.length > 0) {
@@ -49,12 +54,41 @@ export class SearchContext {
           data = [...data, fuse.search(value)]
         }
       })
-      if (_.flattenDeep(data).length === 0) {
+      if (_.flattenDeep(data).length === 0 && byCompanyName.length === 0) {
         return []
       }
-      return _.uniq(_.map(_.flattenDeep(toJS(data)), (data) => data.item))
+      return _.uniq(_.map(_.flattenDeep([toJS(data), byCompanyName]), (data) => data.item))
     }
     return items
   }
+
+  searchByCompanyName = (keySearch, items, searchValue) => {
+    let byCompanyName = []
+    let checkCompanyNameTH = false
+    let checkCompanyNameEN = false
+    const keySearchHasName = _.without(
+      _.map(keySearch, (data) => data.name),
+      undefined
+    )
+
+    if (keySearchHasName.length > 0) {
+      checkCompanyNameTH = _.indexOf(keySearchHasName, 'company_name_th') !== -1
+      checkCompanyNameEN = _.indexOf(keySearchHasName, 'company_name_en') !== -1
+    } else {
+      checkCompanyNameTH = _.indexOf(keySearch, 'company_name_th') !== -1
+      checkCompanyNameEN = _.indexOf(keySearch, 'company_name_en') !== -1
+    }
+
+    if (checkCompanyNameTH || checkCompanyNameEN) {
+      const searchByCompanyName = new Fuse(items, {
+        minMatchCharLength: 3,
+        keys: ['company_name_th', 'company_name_en']
+      })
+      byCompanyName = searchByCompanyName.search(searchValue.join(' '))
+    }
+
+    return byCompanyName
+  }
 }
+
 export const searchContext = createContext(new SearchContext())
